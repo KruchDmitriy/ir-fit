a0a874a96cb272cd389455d99b053d2f1a7e4ad1
KruchDmitriy <kruch.dmitriy@gmail.com>
2017-10-15 01:05:00 +0300

    with bug

--- src/main/java/db/DbConnection.java
+++ src/main/java/db/DbConnection.java
@@ -1,5 +1,6 @@
 package db;
 
+import com.sun.deploy.security.ValidationState;
 import crawl.Page;
 import db.readConfig.Config;
 import org.apache.log4j.Logger;
@@ -63,8 +64,8 @@ public class DbConnection {
     @Nullable
     public ResultSet selectFromUrlByUrl(@NotNull String url, @Nullable String pathToSource) {
         StringBuilder sql = new StringBuilder()
-                .append("SELECT * FROM ULR WHERE ")
-                .append(" URL = ").append(url)
+                .append("SELECT * FROM url_dt WHERE ")
+                .append(" URL like ").append("'" + url + "'")
                 .append(pathToSource == null ? "" : "and path_to_source = " + pathToSource);
         LOGGER.debug(sql.toString());
         try {
@@ -77,7 +78,7 @@ public class DbConnection {
 
     public boolean insertToUrlRow(@NotNull Page page) {
 
-        String sqlInsertToURL = " insert into URL " +
+        String sqlInsertToURL = " insert into url_dt " +
                 "(url, source, source_hash, time_downloading) " +
                 "values (?, ?, ?, ? ); ";
 
@@ -95,7 +96,8 @@ public class DbConnection {
             return false;
         }
 
-        try (PreparedStatement statement = connection.prepareStatement(sqlInsertToURL)) {
+        try (PreparedStatement statement = connection.prepareStatement(sqlInsertToURL,
+                Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, page.getUrl().toString());
             statement.setString(2, pathToFile);
             statement.setInt(3, page.hashCode());
@@ -103,13 +105,19 @@ public class DbConnection {
 
             final int rows = statement.executeUpdate();
             if (rows > 0) {
-                final int urlId = statement.getGeneratedKeys().getInt(1);
-                final ResultSet resultSet = selectFromUrlByUrl(page.getParentUrl().toString(),
+                ResultSet resultSet = statement.getGeneratedKeys();
+                if (resultSet == null || !resultSet.next()) {
+                    throw new SQLException("result set is null");
+
+                }
+                final int urlId = resultSet.getInt(1);
+//                int rowsParent =
+                resultSet = selectFromUrlByUrl(page.getParentUrl().toString(),
                         null);
-                if (resultSet == null) {
-                    LOGGER.fatal(String.format("Parent URL {%s} is not found",
-                            page.getParentUrl().toString()));
-                    return false;
+//                if (rowsParent > 0 ) {
+//                        resultSet = statement.getGeneratedKeys();
+                if (resultSet == null || !resultSet.next()) {
+                    throw new SQLException("result set is null");
                 }
                 final int parentId = resultSet.getInt(1);
 
@@ -119,21 +127,25 @@ public class DbConnection {
                     statement1.setInt(2, parentId);
                     statement1.executeUpdate();
                 } catch (SQLException ex) {
-                    LOGGER.debug(ex.getMessage());
-                    LOGGER.fatal("FAIL sql = " + sqlInsertParent);
-                    connection.rollback();
-                    return false;
+                    throw new SQLException(ex);
                 }
+//                } else  {
+//                    throw new SQLException("0 rows update");
+//                }
 
             } else {
-                connection.rollback();
-                return false;
+                throw new SQLException("0 rows update");
             }
 
             connection.commit();
             return true;
 
         } catch (SQLException ex) {
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                LOGGER.info("rollback fail");
+            }
             LOGGER.debug(ex.getMessage());
             LOGGER.fatal("FAIL sql = " + sqlInsertToURL);
             return false;
@@ -141,7 +153,7 @@ public class DbConnection {
     }
 
     private boolean createURLTable() {
-        StringBuilder sql = new StringBuilder("CREATE TABLE URL (")
+        StringBuilder sql = new StringBuilder("CREATE TABLE IF NOT EXISTS url_dt (")
                 .append(" id serial PRIMARY KEY,\n")
                 .append(" url VARCHAR (10000) UNIQUE NOT NULL,\n")
                 .append(" created_on TIMESTAMP  DEFAULT NOW(),\n")
@@ -151,35 +163,45 @@ public class DbConnection {
                 .append(" time_downloading TIMESTAMP NULL")
                 .append(");");
         try {
-            runExecuteSqlCreateQeury(sql.toString());
+            runExecuteSqlQuery(sql.toString());
             LOGGER.debug("SUCCESS : create url table");
             return true;
 
         } catch (SQLException ex) {
             LOGGER.warn(ex.getMessage());
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                LOGGER.info("rollback error");
+            }
         }
         return false;
     }
 
     private boolean createParentTable() {
         StringBuilder sql = new StringBuilder()
-                .append(" create table parent_url (")
+                .append(" create table IF NOT EXISTS parent_url (")
                 .append(" id serial PRIMARY KEY, \n ")
                 .append(" url_id integer REFERENCES url (id), \n")
                 .append(" parent_id integer REFERENCES url(id) \n")
                 .append(");");
         try {
-            runExecuteSqlCreateQeury(sql.toString());
+            runExecuteSqlQuery(sql.toString());
             LOGGER.debug("SUCCESS : create parent_url table");
             return true;
         } catch (SQLException ex) {
             LOGGER.warn(ex.getMessage());
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                LOGGER.info("rollback error");
+            }
         }
 
         return false;
     }
 
-    private void runExecuteSqlCreateQeury(@NotNull String sql) throws SQLException {
+    private void runExecuteSqlQuery(@NotNull String sql) throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.executeUpdate();
             connection.commit();
@@ -188,8 +210,11 @@ public class DbConnection {
 
     @Nullable
     private ResultSet runExecuteSqlSelectQeury(@NotNull String sql) throws SQLException {
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            return stmt.executeQuery();
+        try (Statement statement = connection.createStatement()) {
+//            if (statement.execute(sql)) {
+//                return null;
+//            }
+            return statement.executeQuery(sql);
         }
     }
 }
