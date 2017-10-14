fbc2a1149882f6c65e307af477309a200ee3d23b
ekaterinaMaljutina <e.maljutina@gmail.com>
2017-10-14 23:03:50 +0300

    fix

--- src/main/java/db/DbConnection.java
+++ src/main/java/db/DbConnection.java
@@ -80,10 +80,10 @@ public class DbConnection {
 
         String sqlInsertToURL = " insert into url_dt " +
                 "(url, source, source_hash, time_downloading) " +
-                "values (?, ?, ?, ? ); ";
+                "values (?, ?, ?, ? ) ";
 
         String sqlInsertParent = "insert into parent_url (url_id, parent_id) " +
-                "values (?, ?); ";
+                "values (?, ?) ";
 
         final String pathToFile = PATH + page.getUrl().toString()
                 .replaceAll("/", "_");
