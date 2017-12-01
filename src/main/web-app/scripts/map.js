2be804ae8a313be4d7d0bb1f979586ea688901ed
ekaterinaMaljutina <e.maljutina@gmail.com>
2017-12-02 00:54:04 +0300

    draw address

--- src/main/web-app/scripts/map.js
+++ src/main/web-app/scripts/map.js
@@ -1,57 +1,51 @@
 ymaps.ready(init);
 
-var map, objects, polygons = [], index = {}, taskId;
-var marker = undefined;
-
-const defaultColor = '#ffff00';
-const goodColor = '#0000ff';
-const badColor = '#ff0000';
-const selectedColor = '#ffffff';
-const selectedGoodColor = '#bbbbff';
-const selectedBadColor = '#ffbbbb';
+var map;
 
 function init() {
-    ymaps.geocode('Нижний Новгород ул.Голубева д3/1', { results: 1 })
-    .then(function (res) {
+
+    map = new ymaps.Map("map", {
+                    center: [0, 0],
+                    zoom: 9
+
+                });
+//    setAddress(['Нижний Новгород ул Голубева д3',
+//                'Нижний Новгород ул Голубева д3/2',
+//                'Нижний Новгород ул Голубева д7'])
+
+}
+
+function setAddress(arrAddress) {
+    arrAddress.forEach(addr => drawLocation(addr));
+}
+
+function drawLocation(address) {
+    ymaps.geocode(address, {
+        results: 1 }
+    ).then(function (res) {
+
         var firstGeoObject = res.geoObjects.get(0);
-        map = new ymaps.Map("map", {
-            center: firstGeoObject.geometry.getCoordinates(),
-            type: "yandex#hybrid",
-            zoom: 18,
-            controls: ['zoomControl']
-        });
+                coords = firstGeoObject.geometry.getCoordinates(),
+                bounds = firstGeoObject.properties.get('boundedBy');
 
-        map.container.fitToViewport();
-//        map.events.add('boundschange', function (e) {
-//            if (e.get('newZoom') !== e.get('oldZoom')) {
-//                if (marker != undefined) {
-//                    if (e.get('newZoom') > 14) {
-//                        map.geoObjects.remove(marker)
-//                    }
-//                    else {
-//                        map.geoObjects.add(marker)
-//                    }
-//                }
-//            }
-//        });
+            firstGeoObject.options.set('preset', 'islands#darkBlueDotIconWithCaption');
+//            firstGeoObject.properties.set('iconCaption', firstGeoObject.getAddressLine());
 
-
-        // Задаем изображение для иконок меток.
-        res.geoObjects.options.set('preset', 'islands#redCircleIcon');
-        res.geoObjects.events
-            // При наведении на метку показываем хинт с названием станции метро.
-            .add('mouseenter', function (event) {
-                var geoObject = event.get('target');
-                myMap.hint.open(geoObject.geometry.getCoordinates(), geoObject.getPremise());
-            })
-            // Скрываем хинт при выходе курсора за пределы метки.
-            .add('mouseleave', function (event) {
-                myMap.hint.close(true);
+            map.geoObjects.add(firstGeoObject);
+            map.setBounds(bounds, {
+                checkZoomRange: true
             });
-        // Добавляем коллекцию найденных геообъектов на карту.
-        myMap.geoObjects.add(res.geoObjects);
-        // Масштабируем карту на область видимости коллекции.
-        myMap.setBounds(res.geoObjects.getBounds());
+
+//             var myPlacemark = new ymaps.Placemark(coords, {
+//                 iconContent: 'моя метка',
+//                 balloonContent: 'Содержимое балуна <strong>моей метки</strong>'
+//                 }, {
+//                 preset: 'islands#violetStretchyIcon'
+//             });
+//
+//             map.geoObjects.add(myPlacemark);
+
+
     }, function(err) {
         console.error(err.message);
     });
